aa9ad675b4a8cb4e887b0ea250a862bec1a5bf08
Michael Zimmermann <m1cha@m1cha-PC.(none)>
2013-02-10 23:50:47 +0100

    initial code commit

---
+++ signzip.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+INPUT="$1"
+OUTPUT="$3"
+KEY="$2"
+
+function printUsage() {
+  echo "Usage: $0 input.zip [key] [output.zip]"
+}
+
+if [ "$INPUT" == "" ];then
+  printUsage
+  exit 1
+fi
+
+if [ "$OUTPUT" == "" ];then
+  filename=$(basename "$INPUT")
+  extension="${filename##*.}"
+  filename="${filename%.*}"
+  OUTPUT="$(pwd)/$filename-signed.$extension"
+fi
+
+if [ "$KEY" == "" ];then
+  KEY="testkey"
+fi
+
+WORKING_DIR="$(pwd)"
+SCRIPT_DIR="$(dirname $(realpath /usr/local/bin/signzip))"
+
+echo "Sign $INPUT as $OUTPUT ..."
+java -Xmx4096m -jar $SCRIPT_DIR/signapk.jar -w $SCRIPT_DIR/$KEY.x509.pem $SCRIPT_DIR/$KEY.pk8 "$INPUT" "$OUTPUT"
+return="$?"
+
+if [ $return -eq 0 ];then
+  echo "Successful!"
+  exit 0
+fi
+
+exit $return
+
